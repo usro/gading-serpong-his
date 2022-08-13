0d719c08b394712f8cf23346d8051485c6232f3d
root <oratoreeza@gmail.com>
2022-08-13 03:19:54 +0000

    Initial commit

---
+++ application/config/license.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = '';
+$config['envato_market_username'] = '';
+$config['SHLK'] = '7E5L36-GPM8DC-H72M72-UDllRnpoQmJQMVEvVTl1ZDM1NnM1VGJCVDUxNnZ4VXhrWUNLcFJ6bjhwWT0=';
